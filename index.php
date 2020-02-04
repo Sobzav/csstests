7819ff1d759f7bc6addde8b3741fcf615fac6f5a
Sobzav <sobzav@gmail.com>
2020-02-04 16:42:59 +0300

    Отдельный css для модального окна modal.css

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" type="text/css" href="css/ant/reset.css">
     <!-- <link rel="stylesheet" href="css/ant/sstyle-orig.css"> -->
     <link rel="stylesheet" href="css/ant/style.css">
+    <link rel="stylesheet" href="css/ant/modal.css">
     <!-- <link rel="stylesheet" href="http://10.30.50.10/webapp/css/style.css"> -->
     <link rel="stylesheet" href="css/fonts.css">
     <link rel="apple-touch-icon" href="icon.png">
@@ -37,8 +38,9 @@
                 <!-- выпадающий список элементов -->
                 <label class="" for="selCode"> Обозначение </label>
                 <select class="select-type" id="selCode" name="package_code">
+
                     <!-- Список всех элементов в базе  -->
-                    <option value=""> Тип </option>
+                    <option id=“0”> Новый </option>
                 </select>
                 <input class="hidden" size="4" type="text" id="inpCodeEdit" name="package_code">
             </div>
@@ -185,9 +187,9 @@
                 <section class="section__subsumm">
 
                     <div class="">
-                        <label class="" for="inpTotal4"> Общее количество = </label>
-                        <label class="" type="text" id="inpTotal4" name="package_total4"> 0 </label>
-                        <label class="" for="inpTotal4"> шт </label>
+                        <label class="" for="inpTotal"> Общее количество = </label>
+                        <label class="" type="text" id="inpTotal" name="package_total"> 0 </label>
+                        <label class="" for="inpTotal"> шт </label>
                     </div>
 
                 </section>
@@ -226,7 +228,53 @@
     <script src="js/jquery.validate.js"></script>
     <!-- <script src="js/main.js"></script> -->
     <script src="http://10.30.50.10/webapp/package/main.js"></script>
+
 </body>
+
+<!-- MODAL DIALOG OPEN -->
+<div class="dialog-modal">
+    <!-- - block -->
+    <!-- <div class="gridContainer__modal modal-alert"> Если красный цвет -->
+    <!-- <div class="gridContainer__modal modal-info"> Если синий цвет -->
+    <div class="center-modal">
+        <div class="gridContainer__modal">
+            <!-- inline-block; -->
+            <header class="header-top__modal">
+
+                <!-- TOP - Панель с заголовком -->
+                <div id="title" class=""> Заголовок id="title" </div>
+
+            </header>
+            <!-- Контейнер диалогового окна -->
+            <div class="dialog-form">
+
+                <header class="header__modal">
+                    <div id="header" class="label-heading"> Подзаголовок id="header" </div>
+                </header>
+
+                <section id="mess" class="section-modal">
+                    <div>
+                        <p id="" class=""><span style="font-weight: bold;">id="mess"</span> - Сообщение на одну строчку Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est quia consectetur autem eligendi beatae. Cupiditate tenetur molestiae officia aliquid numquam quam molestias fugiat distinctio eaque. Minus suscipit corporis placeat, voluptates id libero quod rerum perferendis officiis? In nisi nam ab. Fuga nemo reiciendis quasi vitae consectetur, voluptatem praesentium consequuntur excepturi deserunt. Sunt sint natus facilis ipsam soluta fuga accusamus maxime quisquam consequatur, reprehenderit aut maiores reiciendis corrupti rem nulla dicta beatae ipsa officia doloribus error magnam ab, iusto consectetur exercitationem. A consectetur maxime aliquid qui tempora rem explicabo quia nam, quibusdam voluptatibus voluptatum. Ratione error facilis alias culpa sit consectetur a vitae delectus dolore cupiditate, quas, nihil magnam itaque ipsam, quasi iste porro voluptates nemo. Illum totam nisi non vero ratione voluptatibus eaque temporibus animi fugit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis atque dolor, incidunt molestias omnis fugit eius velit, vel voluptatibus, quas obcaecati tempore accusamus! Assumenda reprehenderit odit debitis tenetur corrupti nisi vero, sunt repellendus deleniti voluptates et ad blanditiis, cum eaque, commodi quia veritatis consectetur sequi fuga dolorem porro ducimus non facilis. Aliquid ducimus nisi, amet voluptatibus commodi </p>
+                    </div>
+                </section>
+
+                <!-- Блок кнопок -->
+                <section class="bottom-block__buttons--modal">
+
+                    <button class="button-right" id="btnOk_modal"> OK </button>
+                    <button class="button-right" disabled="disabled" id="btnCancel_modal"> Cancel </button>
+                    <!-- <button class="" id="btnAdd"> Добавить </button> -->
+                    <button class="button-right" id="btnClose_modal"> Close </button>
+
+                </section>
+
+            </div>
+        </div>
+    </div>
+</div>
+<!-- MODAL DIALOG END -->
+
+
 
 <footer>
 </footer>
--- modal.php
+++ modal.php
@@ -6,49 +6,60 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="favicon.ico" type="image/x-icon">
     <link rel="stylesheet" type="text/css" href="css/ant/reset.css">
-    <!-- <link rel="stylesheet" href="css/ant/style-orig.css"> -->
+
     <link rel="stylesheet" href="css/ant/style.css">
-    <!-- <link rel="stylesheet" href="http://10.30.50.10/webapp/css/style.css"> -->
+
     <link rel="stylesheet" href="css/fonts.css">
+    <link rel="stylesheet" href="css/ant/modal.css">
     <link rel="apple-touch-icon" href="icon.png">
     <title> Модальное окно </title>
 </head>
 
 <body class="">
 
-    <!-- <div class="gridContainer__modal modal-alert"> -->
-    <div class="gridContainer__modal" >
-        <header class="header-top__modal">
+<!-- MODAL DIALOG OPEN -->
+<div class="dialog-modal">
+    <!-- - block -->
+    <!-- <div class="gridContainer__modal modal-alert"> Если красный цвет -->
+    <!-- <div class="gridContainer__modal modal-info"> Если синий цвет -->
+    <div class="center-modal">
+        <div class="gridContainer__modal">
 
-            <!-- TOP - Панель с заголовком -->
-            <div id="title" class=""> Заголовок id="title" </div>
+            <header class="header-top__modal">
 
-        </header>
-        <!-- Изображение и характеристики -->
-        <main role="main">
+                <!-- TOP - Панель с заголовком -->
+                <div id="title" class=""> Заголовок id="title" </div>
 
-            <header class="header__modal">
-                <div id="header" class="label-heading"> Подзаголовок id="header" </div>
             </header>
+            <!-- Контейнер диалогового окна -->
+            <div class="dialog-form">
 
-            <section id="mess" class="section-modal">
-                <div>
-                    <p id="" class=""><span style="font-weight: bold;">id="mess"</span> - Сообщение на одну строчку</p>
-                </div>
-            </section>
+                <header class="header__modal">
+                    <div id="header" class="label-heading"> Подзаголовок id="header" </div>
+                </header>
 
-            <!-- кнопки -->
-            <section class="bottom-block__buttons--modal">
+                <section id="mess" class="section-modal">
+                    <div>
+                        <p id="" class=""><span style="font-weight: bold;">id="mess"</span> - Сообщение на одну строчку Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est quia consectetur autem eligendi beatae. Cupiditate tenetur molestiae officia aliquid numquam quam molestias fugiat distinctio eaque. Minus suscipit corporis placeat, voluptates id libero quod rerum perferendis officiis? In nisi nam ab. Fuga nemo reiciendis quasi vitae consectetur, voluptatem praesentium consequuntur excepturi deserunt. Sunt sint natus facilis ipsam soluta fuga accusamus maxime quisquam consequatur, reprehenderit aut maiores reiciendis corrupti rem nulla dicta beatae ipsa officia doloribus error magnam ab, iusto consectetur exercitationem. A consectetur maxime aliquid qui tempora rem explicabo quia nam, quibusdam voluptatibus voluptatum. Ratione error facilis alias culpa sit consectetur a vitae delectus dolore cupiditate, quas, nihil magnam itaque ipsam, quasi iste porro voluptates nemo. Illum totam nisi non vero ratione voluptatibus eaque temporibus animi fugit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis atque dolor, incidunt molestias omnis fugit eius velit, vel voluptatibus, quas obcaecati tempore accusamus! Assumenda reprehenderit odit debitis tenetur corrupti nisi vero, sunt repellendus deleniti voluptates et ad blanditiis, cum eaque, commodi quia veritatis consectetur sequi fuga dolorem porro ducimus non facilis. Aliquid ducimus nisi, amet voluptatibus commodi </p>
+                    </div>
+                </section>
 
-                <button class="button-right" id="btnCopy"> OK </button>
-                <button class="button-right" id="btnDelete"> Cancel </button>
-                <!-- <button class="" id="btnAdd"> Добавить </button> -->
-                <button class="button-right" id="btnApply"> Close </button>
+                <!-- Блок кнопок -->
+                <section class="bottom-block__buttons--modal">
 
-            </section>
+                    <button class="button-right" id="btnOk_modal"> OK </button>
+                    <button class="button-right" disabled="disabled" id="btnCancel_modal"> Cancel </button>
+                    <!-- <button class="" id="btnAdd"> Добавить </button> -->
+                    <button class="button-right" id="btnClose_modal"> Close </button>
 
-        </main>
+                </section>
+
+            </div>
+        </div>
     </div>
+</div>
+<!-- MODAL DIALOG END -->
+
     <!--скрипты для стилей -->
     <!-- <script src="js/jquery-3.4.1.min.js"></script>
     <script src="js/jquery.validate.js"></script>
